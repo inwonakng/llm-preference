ecfbec75f5a50102121588220bf067523d1a07b7
inwonakng <inwon.kang04@gmail.com>
2023-09-24 21:01:07 -0400

    adding fine-tuning code

---
+++ lang_pref/llm/fine_tuning/test.py
@@ -0,0 +1,30 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+from pathlib import Path
+
+
+model_path = Path('~/text-generation-webui/models/TheBloke_Upstage-Llama-2-70B-instruct-v2-GPTQ_gptq-4bit-32g-actorder_True')
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    device_map="auto",
+    trust_remote_code=False,
+    revision="main"
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+print('loaded model successfully!')
