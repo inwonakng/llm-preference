c32e8859e45025cbb684e0e32353262df945fd34
inwonakng <inwon.kang04@gmail.com>
2024-01-10 17:18:06 -0500

    modified to save indiv files for easier checkpointing

--- synthetic_generation/run.py
+++ synthetic_generation/run.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import random
 import yaml
 import click
+import json
 
 import numpy as np
 import pandas as pd
@@ -14,8 +15,7 @@ import openai
 
 openai.api_key = "sk-111111111111111111111111111111111111111111111111"
 openai.api_base = "http://0.0.0.0:5000/v1"
-
-CHECKPOINT_ROWS = 10
+np.random.seed(0)
 
 def progress_bar():
     return rich.progress.Progress(
@@ -45,18 +45,15 @@ DATASET_TO_PATH = {
 @click.command()
 @click.argument("dataset_name", default="compsent")
 @click.option("--num_rows", default=10000)
-# @click.option('--mode', default='all')
 def run(
     dataset_name: str,
     num_rows: int,
-    # output_file: str
 ):
-    output_file = f"{dataset_name}.csv"
+    output_dir = Path(f"outputs/{dataset_name}")
+    output_dir.mkdir(exist_ok=True, parents=True)
     template = f"templates/{dataset_name}.yaml"
     config = yaml.safe_load(open(template))
-    # dataset = config["dataset"]
     df = pd.read_csv(DATASET_TO_PATH[dataset_name])
-    # alternatives_selector = DATASET_TO_ALTERNATIVE_FUNCTION[dataset_name]
 
     prompt = Prompt.load_template(
         template,
@@ -70,13 +67,15 @@ def run(
             description="Generating rows...", total=num_rows
         )
         for i in range(num_rows):
-            print(Rule())
-            alternative_a, alternative_b = choose_alternatives(df)
-            label, output, prompt_kwargs, instruction, task = prompt.execute(
-                alternative_a=alternative_a, alternative_b=alternative_b
-            )
-            rows.append(
-                {
+            filename = output_dir / f"{i:08d}.json"
+            if not filename.is_file():
+                print(Rule())
+                alternative_a, alternative_b = choose_alternatives(df)
+                label, output, prompt_kwargs, instruction, task = prompt.execute(
+                    alternative_a=alternative_a, alternative_b=alternative_b
+                )
+
+                one_row = {
                     "Label": label,
                     "Alt_a": alternative_a,
                     "Alt_b": alternative_b,
@@ -85,14 +84,8 @@ def run(
                     "Task": task,
                     **prompt_kwargs,
                 }
-            )
-
-            if (i + 1) % CHECKPOINT_ROWS == 0:
-                pd.DataFrame(rows).to_csv(output_file, index=False)
-
+                json.dump(one_row, open(filename,"w"), indent=2)
             progress.update(progress_task, advance=1)
-
-    pd.DataFrame(rows).to_csv(output_file, index=False)
 
 if __name__ == "__main__":
     run()
